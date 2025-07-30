592ddb7cc3f252aaa0d3679044878bfeeb084d64
bgirot <balthazar.girot@gmail.com>
2025-07-30 21:28:15 +0200

    routes working

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -2,11 +2,10 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Footer } from './components/footer/footer';
 import { Header } from './components/header/header';
-import { Wip } from './components/wip/wip';
 
 @Component({
   selector: 'app-root',
-  imports: [Footer, Header, Wip],
+  imports: [Footer, Header, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ frontend/src/app/pages/home/home.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Wip } from '../../components/wip/wip';
+
+@Component({
+  selector: 'app-home',
+  imports: [Wip],
+  templateUrl: './home.html',
+  styleUrl: './home.scss'
+})
+export class Home {
+
+}
