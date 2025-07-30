51fcb00fa431cd5600b9200aa798336d99f63394
bgirot <balthazar.girot@gmail.com>
2025-07-30 21:54:43 +0200

    default-btn component

--- frontend/src/app/components/wip/wip.ts
+++ frontend/src/app/components/wip/wip.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { APP_CONSTANTS } from '../../constants/constants';
+import { DefaultBtn } from '../default-btn/default-btn';
 
 @Component({
     selector: 'app-wip',
-    imports: [],
+    imports: [DefaultBtn],
     templateUrl: './wip.html',
     styleUrl: './wip.scss'
 })
