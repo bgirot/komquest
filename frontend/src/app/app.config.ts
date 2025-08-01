5ccdfa1d2a792e3d97ce4eb6090a0c2dded84e33
bgirot <balthazar.girot@gmail.com>
2025-08-02 01:34:01 +0200

    fix not scrolling on page change

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, LOCALE_ID, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling, withRouterConfig } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { importProvidersFrom } from '@angular/core';
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
@@ -15,7 +15,16 @@ export function HttpLoaderFactory(http: HttpClient): TranslateLoader {
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideRouter(
+      routes,
+      withInMemoryScrolling({
+        scrollPositionRestoration: 'top',
+        anchorScrolling: 'enabled'
+      }),
+      withRouterConfig({
+        onSameUrlNavigation: 'reload'
+      })
+    ),
     provideHttpClient(),
     importProvidersFrom(
       TranslateModule.forRoot({
