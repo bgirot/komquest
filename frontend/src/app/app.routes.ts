4294451bf7914f1d30e950e82c278e0c3c356a7e
bgirot <balthazar.girot@gmail.com>
2025-08-02 23:17:00 +0200

    privacy done (still needs to be redacted)

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { About } from './pages/about/about';
 import { Legals } from './pages/legals/legals';
+import { Privacy } from './pages/privacy/privacy';
 
 export const routes: Routes = [
     {
@@ -17,5 +18,10 @@ export const routes: Routes = [
     {
         path: 'legals',
         component: Legals
+    },
+
+    {
+        path: 'privacy',
+        component: Privacy
     }
 ];
---
+++ frontend/src/app/pages/privacy/privacy.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+import { TranslateModule } from "@ngx-translate/core";
+
+@Component({
+  selector: 'app-privacy',
+  imports: [TranslateModule],
+  templateUrl: './privacy.html',
+  styleUrls: ['./privacy.scss']
+})
+export class Privacy {
+
+}
