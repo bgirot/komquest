c70abf64f5b903f0d34948e1bdc124a91d97b197
bgirot <balthazar.girot@gmail.com>
2025-08-02 23:52:24 +0200

    better header behaviour

--- frontend/src/app/components/header/header.ts
+++ frontend/src/app/components/header/header.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { LanguageService } from '../../services/language.service';
 
@@ -8,8 +8,37 @@ import { LanguageService } from '../../services/language.service';
   templateUrl: './header.html',
   styleUrl: './header.scss'
 })
-export class Header {
+export class Header implements OnInit {
+  private lastScrollTop = 0;
+  private scrollThreshold = 5;
+  isHeaderVisible = true;
+
   constructor(private languageService: LanguageService) {}
+
+  ngOnInit(): void {
+    this.lastScrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll(event: Event): void {
+    const currentScrollTop = window.pageYOffset || document.documentElement.scrollTop;
+   
+    // for micro movements
+    if (Math.abs(currentScrollTop - this.lastScrollTop) < this.scrollThreshold) {
+      return;
+    }
+
+    // If we scroll down and we arent on top
+    if (currentScrollTop > this.lastScrollTop && currentScrollTop > 100) {
+      this.isHeaderVisible = false;
+    } 
+    // If we scroll up
+    else if (currentScrollTop < this.lastScrollTop) {
+      this.isHeaderVisible = true;
+    }
+
+    this.lastScrollTop = currentScrollTop;
+  }
 
   switchLanguage(): void {
     this.languageService.switchLanguage();
